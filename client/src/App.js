c8360724e0319a07c1ce206933e881137a889d4e
miten09 <mitensoni108@gmail.com>
2023-06-19 19:52:28 +0530

    front start

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
